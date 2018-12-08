68f60f055aeab96fc15e24f41596b326bd9214fe
bomi1490 <bomi1490@gmail.com>
2018-12-09 02:49:50 +0900

    ExitPage

--- app/src/main/java/com/example/bomi1/myyummylunch1/ExitPage.java
+++ app/src/main/java/com/example/bomi1/myyummylunch1/ExitPage.java
@@ -1,14 +1,19 @@
 package com.example.bomi1.myyummylunch1;
 
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ExitPage extends AppCompatActivity{
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exit);
+
+        ActivityCompat.finishAffinity(this);
+
     }
 }
